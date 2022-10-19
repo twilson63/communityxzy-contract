57e94b27f2a8f2e153ab7a60aae0984b04a01ad0
Tom Wilson <tom@hyper.io>
2022-10-19 16:23:08 +0000

    update deploy.js

---
+++ deploy.js
@@ -0,0 +1,32 @@
+const { WarpFactory, LoggerFactory, defaultCacheOptions } = require('warp-contracts')
+const fs = require('fs')
+const Arweave = require('arweave')
+
+const wallet = JSON.parse(fs.readFileSync('./wallet.json', 'utf-8'))
+
+LoggerFactory.INST.logLevel('error')
+const arweave = Arweave.init({ host: 'arweave.net', port: 443, protocol: 'https' })
+const warp = WarpFactory.custom(arweave, {
+  ...defaultCacheOptions,
+  inMemory: true
+}, 'mainnet')
+  .useArweaveGateway()
+  .build()
+
+async function main() {
+  const initState = warp.contract(VOUCH).readState().then(res => res.cacheValue.state)
+  console.log(initState)
+  // TODO: Change old Wallet Address with new Wallet Address
+  const src = fs.readFileSync('./dist/contract.js', 'utf-8')
+
+  const result = await warp.createContract.deploy({
+    src,
+    wallet,
+    initState: JSON.stringify(initState)
+  })
+
+  console.log(result)
+
+}
+
+main()
